6d977a4e8745810a39be38d3998c31997a8ad4d1
Anna De Lise <anna_delise@libero.it>
2025-08-06 16:43:15 +0200

    add responsive toggle navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,31 @@
-import { Nav } from "react-bootstrap"
+ import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from "react-router-dom"
 
 const Navb = ()=> {
     return(
-        <>
-      <Nav className=" justify-content-center  bg-info border-bottom border-black" activeKey="/home">
-    <Nav.Item className="d-flex align-items-center">
+<>
+  <Navbar expand="md" bg="info" variant="light" className="border-bottom border-black">
+    <Container className="justify-content-center">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mx-auto d-flex align-items-center">
           <Nav.Link as={Link} to="/" className="title fs-3 text-dark-emphasis" eventKey="link-1">
             Home
           </Nav.Link>
-          <Nav.Link as={Link} to="/"  eventKey="link-1">
-           <img id="logo" src="public/workielogo.png" alt="logo" className="mx-4" />
+
+          <Nav.Link as={Link} to="/" eventKey="link-1">
+            <img id="logo" src="public/workielogo.png" alt="logo" className="mx-4" />
           </Nav.Link>
+
           <Nav.Link as={Link} to="/favourites" className="text-dark-emphasis title fs-3" eventKey="link-2">
             Favourites
           </Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+</>
+
     )
 }
 export default Navb
