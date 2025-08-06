6e6b98fe6bbac80eedd94ba5b301ce194ff0566b
Anna De Lise <anna_delise@libero.it>
2025-08-06 16:34:17 +0200

    add

--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -3,7 +3,7 @@ import { Container, Row } from "react-bootstrap";
 const Foot = () => {
   return (
     <>
-      <Container fluid className="bg-info">
+      <Container fluid className="bg-info border-top border-black">
         <Row className="text-end me-5 p-2">
           <p className="title fs-3 text-dark-emphasis m-0">Workie &copy;{new Date().getFullYear}</p>
         </Row>
